4bad0c080dc1a70079fb3da1a1ee0cb5a0480ef2
ahn <34005957+glingi@users.noreply.github.com>
2018-06-03 23:09:51 +0900

    Update tb_top_256fft.cpp

--- HLS/tb_top_256fft.cpp
+++ HLS/tb_top_256fft.cpp
@@ -1,5 +1,5 @@
 //
-// Testbench
+// C/RTL cosimulation testbench
 //
 
 #include "top_256fft.h"
@@ -10,16 +10,14 @@ int main()
 {
 	int nSegment = 32;
 
-	ifix_W10_F9 fft_in_set[NFFT*nSegment];
+	ifix_W10_F9  fft_in_set[NFFT*nSegment];
 	ifix_W10_F9  fft_in[NFFT*nSegment];
 
 	cfix_W14_F8  fft_HLS_set[NFFT*nSegment];
 	cfix_W14_F8  fft_HLS[NFFT*nSegment];
-//	cfix_W22_F13 fft_HLS[NFFT];
-
+	
 	ifstream din_s_real;
 	din_s_real.open("./mid_fft_input_ch1.dat");
-
 	if(din_s_real.is_open()){
 
 		for(int c = 0; c < NFFT*nSegment; c++){
@@ -31,27 +29,25 @@ int main()
 	}
 	din_s_real.close();
 
-//	std::cout << "[[tb_top_256fft.cpp] --------------- fft input " << std::endl;
-//	for(int c = 0; c < NFFT*nSegment; c++)
-//		std::cout << fft_in_set[c] << std::endl;
-
-//	s[0].real() = 0.0008544922;	// it doesn't work
-//	s[64].real() = 0.0013427734;
+	// Below codes doesn't work		
+	// s[0].real() = 0.0008544922;	
+	// s[64].real() = 0.0013427734;
 
 	for(int iter=0; iter<nSegment; iter++){
 		cout << " ------------------- iteration : " << iter << "---------------" << endl;
 
-		for(int idx=0; idx < NFFT; idx++)
+		for(int idx=0; idx < NFFT; idx++){
 			fft_in[idx] = fft_in_set[NFFT*iter + idx];
-
+		}			
 
 		radix4FFT3_FixPtEML(
-				fft_in,
-				fft_HLS
+			fft_in,
+			fft_HLS
 		);
 
-		for(int idx=0; idx < NFFT; idx++)
+		for(int idx=0; idx < NFFT; idx++){
 			fft_HLS_set[NFFT*iter + idx] = fft_HLS[idx];
+		}
 	}
 
 	ofstream dout_s_real;
